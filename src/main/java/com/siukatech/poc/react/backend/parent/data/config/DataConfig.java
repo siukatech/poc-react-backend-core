10fab9b266d0f952e40d0471f02dff973c4ea0a7
siukatech <siuka114@gmail.com>
2023-07-15 16:05:09 +0800

    put auditorAware from app project to parent project

--- src/main/java/com/siukatech/poc/react/backend/parent/data/config/DataConfig.java
+++ src/main/java/com/siukatech/poc/react/backend/parent/data/config/DataConfig.java
@@ -1,20 +1,42 @@
 package com.siukatech.poc.react.backend.parent.data.config;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 ////@AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
 ////@EnableTransactionManagement(proxyTargetClass = true)
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EntityScan(basePackages = {"com.siukatech.poc.react.backend.parent.data.entity"})
 @EnableJpaRepositories("com.siukatech.poc.react.backend.parent.data.repository")
 //@ComponentScan(basePackages = { "com.siukatech.poc.react.backend.parent.data" })
 ////@Import(StarterEntityRegistrar.class)
 public class DataConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAware<String>() {
+            private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+            @Override
+            public Optional<String> getCurrentAuditor() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                String name = (authentication == null ? "NULL" : authentication.getName());
+                logger.debug("getCurrentAuditor - name: [{}], authentication: [{}]"
+                        , name, authentication);
+                return Optional.of(name);
+            }
+        };
+    }
+
 }
