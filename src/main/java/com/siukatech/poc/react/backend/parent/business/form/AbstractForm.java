eaee7e0b8b478edaea74872c42b9b799db3de87f
siukatech <siuka114@gmail.com>
2024-01-07 19:38:20 +0800

    add generic type of id to AbstractForm

--- src/main/java/com/siukatech/poc/react/backend/parent/business/form/AbstractForm.java
+++ src/main/java/com/siukatech/poc/react/backend/parent/business/form/AbstractForm.java
@@ -11,7 +11,10 @@ import lombok.Setter;
 @MappedSuperclass
 // https://stackoverflow.com/a/5455563
 //@JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class AbstractForm {
+public abstract class AbstractForm<T> {
+
+    protected T id;
+
     @NotNull(message = "Version No cannot be NULL")
     protected Long versionNo;
 }
