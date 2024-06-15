bf4652d4bf427ab142a8489cf6811b67c19a1724
siukatech <siuka114@gmail.com>
2024-06-16 01:25:52 +0800

    feat: add current class name to log

--- src/main/java/com/siukatech/poc/react/backend/parent/data/listener/AbstractEntityToPersistListener.java
+++ src/main/java/com/siukatech/poc/react/backend/parent/data/listener/AbstractEntityToPersistListener.java
@@ -11,9 +11,13 @@ public class AbstractEntityToPersistListener {
 
     @PrePersist
     protected void onSavePrePersist(final AbstractEntity abstractEntity) {
-        log.debug("onSavePrePersist - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePrePersist - abstractEntity.getClass().getName: [{}"
+                + "], abstractEntity.getId: [{}"
+                + "], abstractEntity.getVersionNo: [{}"
+                + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
         if (abstractEntity.getId() != null && abstractEntity.getVersionNo() == null) {
             // since the version is null, it treats "CREATE"
             String message = "VersionNo cannot null be null if abstractEntity.id#" + abstractEntity.getId() + " exists, onSavePrePersist";
@@ -23,16 +27,24 @@ public class AbstractEntityToPersistListener {
 
     @PostPersist
     protected void onSavePostPersist(final AbstractEntity abstractEntity) {
-        log.debug("onSavePostPersist - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePostPersist - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
     }
 
     @PreUpdate
     protected void onSavePreUpdate(final AbstractEntity abstractEntity) {
-        log.debug("onSavePreUpdate - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePreUpdate - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
         if (abstractEntity.getVersionNo() == null) {
             throw new IllegalArgumentException("VersionNo cannot null be null for " + abstractEntity.getClass().getName() + ".id#" + abstractEntity.getId() + ", onSavePreUpdate");
         }
@@ -40,30 +52,46 @@ public class AbstractEntityToPersistListener {
 
     @PostUpdate
     protected void onSavePostUpdate(final AbstractEntity abstractEntity) {
-        log.debug("onSavePostUpdate - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePostUpdate - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
     }
 
     @PreRemove
     protected void onSavePreRemove(final AbstractEntity abstractEntity) {
-        log.debug("onSavePreRemove - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePreRemove - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
     }
 
     @PostRemove
     protected void onSavePostRemove(final AbstractEntity abstractEntity) {
-        log.debug("onSavePostRemove - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onSavePostRemove - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
     }
 
     @PostLoad
     protected void onGetPostLoad(final AbstractEntity abstractEntity) {
-        log.debug("onGetPostLoad - abstractEntity.getId: [" + abstractEntity.getId()
-                + "], abstractEntity.getVersionNo: [" + abstractEntity.getVersionNo()
-                + "]");
+        log.debug("onGetPostLoad - abstractEntity.getClass().getName: [{}"
+                        + "], abstractEntity.getId: [{}"
+                        + "], abstractEntity.getVersionNo: [{}"
+                        + "]"
+                , abstractEntity.getClass().getName()
+                , abstractEntity.getId()
+                , abstractEntity.getVersionNo());
     }
 
 }
