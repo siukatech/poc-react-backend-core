c66335b5565c254461e43d2b60f3e7289d41e0cd
siukatech <siuka114@gmail.com>
2025-01-29 23:20:40 +0800

    feat: add AbstractUserEntity for UserEntity and UserViewEntity

---
+++ src/main/java/com/siukatech/poc/react/backend/core/user/entity/AbstractUserEntity.java
@@ -0,0 +1,29 @@
+package com.siukatech.poc.react.backend.core.user.entity;
+
+import com.siukatech.poc.react.backend.core.data.entity.AbstractEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class AbstractUserEntity extends AbstractEntity<String> {
+
+    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    protected String id;
+
+    @Column(name = "login_id")
+    protected String loginId;
+
+    @Column
+    protected String name;
+
+    @Column(name = "public_key")
+    protected String publicKey;
+
+    @Column(name = "private_key")
+    protected String privateKey;
+
+}
