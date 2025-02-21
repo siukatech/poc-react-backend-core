be06c051f0e3dbaf42fc0edfdccb04daabec623d
siukatech <siuka114@gmail.com>
2025-02-22 01:07:48 +0800

    fix: add back redis properties support in RedisCachingConfig for ConnectionFactory

--- src/main/java/com/siukatech/poc/react/backend/core/caching/config/RedisCachingConfig.java
+++ src/main/java/com/siukatech/poc/react/backend/core/caching/config/RedisCachingConfig.java
@@ -1,12 +1,10 @@
 package com.siukatech.poc.react.backend.core.caching.config;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +12,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.util.Objects;
 
 @Slf4j
 @Configuration
@@ -36,11 +37,21 @@ public class RedisCachingConfig extends AbstractCachingConfig {
 //    }
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory(
-            RedisProperties redisProperties) {
-        return new LettuceConnectionFactory(
-                redisProperties.getHost(),
-                redisProperties.getPort());
+    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        if (Objects.nonNull(redisProperties.getPassword())) {
+            redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        }
+        redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
+        if (Objects.nonNull(redisProperties.getLettuce())) {
+            // do something
+        }
+        log.debug("redisConnectionFactory - redisStandaloneConfiguration: [${}]"
+                        + ", redisProperties: [{}]"
+                , redisStandaloneConfiguration, redisProperties);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
